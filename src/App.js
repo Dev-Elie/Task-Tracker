2348434cb426946145aaa2940f0f465b043d9605
Dev-Elie <eochieng9448@gmail.com>
2021-07-03 13:16:33 +0300

    added routes

--- src/App.js
+++ src/App.js
@@ -86,14 +86,18 @@ const App = () => {
         <div className="container">
 
       {/* Toggle the add form on button click */}
-      <Header onAdd={()=>setShowAddTask(!showAddTask)}
-      showAdd ={showAddTask}
-       />
+      <Header onAdd={()=>setShowAddTask(!showAddTask)} showAdd ={showAddTask}/>
 
-      {showAddTask && <AddTask onAdd ={addTask} />}
+      <Route path= '/' exact render={(props)=>(
+        <>
+            {showAddTask && <AddTask onAdd ={addTask} />}
+            {tasks.length > 0 ? (<Tasks tasks= {tasks} 
+            onDelete = {deleteTask} 
+            onToggle={toggleReminder} />):('No tasks yet')}
+        </>
 
-
-      {tasks.length > 0 ? (<Tasks tasks= {tasks} onDelete = {deleteTask} onToggle={toggleReminder} />):('No tasks yet')}
+        )}
+      />
       <Route path= '/about' component={About} />
       < Footer />
     </div>
